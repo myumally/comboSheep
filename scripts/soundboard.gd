1cd4e47c3dc66559853499c8b933e25804a936bc
myumally <domitillevale@gmail.com>
2026-06-14 13:19:00 +0200

    bug2manette effect works

--- scripts/soundboard.gd
+++ scripts/soundboard.gd
@@ -23,6 +23,10 @@ var effects: Array[SoundData]
 var animals: Array[SoundData]
 # liste des sons actifs
 var actives: Array[SoundData]
+# liste des sons à activer
+var to_active: Array[SoundData]
+
+var current_layout: int
 
 func _ready():
 	# initialisation des sons
@@ -40,14 +44,14 @@ func _ready():
 	
 	effects = [thunder, gun, sword, explosion]
 	animals = [horse, chicken, wolf, goat, cat]
-	actives = [horse, chicken, cat, thunder, laugh, bug2manette]
 	
-	var i = 0
 	# initialisation des buttons
 	for button in get_children():
 		button.sound_selected.connect(_on_sound_selected)
-		button.set_sound(actives[i])
-		i += 1
+	
+	# initialisation de la soundboard
+	current_layout = 0
+	update_soundboard()
 
 func _on_sound_selected(id: SoundId.SoundId):
 	sound_selected.emit(id)
@@ -65,25 +69,41 @@ func _on_sound_selected(id: SoundId.SoundId):
 		SoundId.SoundId.BUG2MANETTE:
 			_on_bug()
 
-func _on_laugh() -> void: #todo faire du pseudo-alétoire parce que trop chiant là
-	actives = [laugh, bug2manette]
+func _on_laugh() -> void:
+	current_layout = (current_layout+1)%12
+	update_soundboard()
+
+func _on_bug() -> void:
+	for i in range(4):
+		animals[i].setId(animals[(i+1)%4]._id)
+
+func select_sounds():
+	to_active = [laugh, bug2manette]
 	for i in range(2):
 		var animal = animals.pick_random()
-		while animal in actives:
+		while animal in to_active:
 			animal = animals.pick_random()
-		actives.append(animal)
+		to_active.append(animal)
 		var effect = effects.pick_random()
-		while effect in actives:
+		while effect in to_active:
 			effect = effects.pick_random()
-		actives.append(effect)
-	actives.shuffle()
+		to_active.append(effect)
+
+func select_layout(l: int):
+	actives = []
+	var decal = l%6
+	for i in range(6):
+		actives.append(to_active[(i+decal)%6])
+	if l >= 6:
+		actives.reverse()
+
+func update_soundboard():
+	select_sounds()
+	select_layout(current_layout)
 	var i = 0
 	for button in get_children():
 		button.set_sound(actives[i])
 		i += 1
 
-func _on_bug() -> void:
-	pass
-
-func update(id: SoundId.SoundId, stream: AudioStream, button: Button ) -> void:
+func update_button(id: SoundId.SoundId, stream: AudioStream, button: Button ) -> void:
 	button.update(id, stream)
